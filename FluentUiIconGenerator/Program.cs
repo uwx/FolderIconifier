7c33fabcf5636e90a8b89fea2014f0e565ad2055
uwx <maxine@cybergal.com>
2025-01-18 15:59:29 -0300

    initial commit

---
+++ FluentUiIconGenerator/Program.cs
@@ -0,0 +1,134 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+Console.WriteLine("Hello, World!");
+
+const string emojiRoot = @"G:\GitHub\Csharp\FolderIconifier\FolderIconifier\Icons\Emoji";
+
+foreach (var emojiDir in Directory.GetDirectories(@"G:\GitHub\Misc\fluentui-emoji\assets"))
+{
+    if (!Directory.Exists(Path.Combine(emojiDir, "3D")))
+    {
+        Console.WriteLine($"Skin tone emoji: {emojiDir}");
+        continue;
+    }
+    
+    var metadataPath = Path.Combine(emojiDir, "metadata.json");
+    var metadata = JsonSerializer.Deserialize<EmoteMetadata>(File.ReadAllText(metadataPath))!;
+    var file = Directory.GetFiles(Path.Combine(emojiDir, "3D"), "*.png")[0];
+    if (!IconHelper.ConvertToIcon(file, Path.Combine(emojiRoot, metadata.Cldr + ".ico"), 256))
+    {
+        throw new InvalidOperationException($"Failed to convert {file} to ico");
+    }
+}
+
+public record EmoteMetadata(
+    [property: JsonPropertyName("cldr")] string Cldr,
+    [property: JsonPropertyName("fromVersion")] string FromVersion,
+    [property: JsonPropertyName("glyph")] string Glyph,
+    [property: JsonPropertyName("group")] string Group,
+    [property: JsonPropertyName("keywords")] string[] Keywords,
+    [property: JsonPropertyName("mappedToEmoticons")] string[] MappedToEmoticons,
+    [property: JsonPropertyName("tts")] string Tts,
+    [property: JsonPropertyName("unicode")] string Unicode
+);
+
+/// <summary>
+/// Provides helper methods for imaging
+/// </summary>
+public static class IconHelper
+{
+    /// <summary>
+    /// Converts a PNG image to a icon (ico)
+    /// </summary>
+    /// <param name="input">The input stream</param>
+    /// <param name="output">The output stream</param>
+    /// <param name="size">Needs to be a factor of 2 (16x16 px by default)</param>
+    /// <param name="preserveAspectRatio">Preserve the aspect ratio</param>
+    /// <returns>Wether or not the icon was succesfully generated</returns>
+    public static bool ConvertToIcon(Stream input, Stream output, int size = 16, bool preserveAspectRatio = false)
+    {
+        if (Image.FromStream(input) is not Bitmap inputBitmap)
+            return false;
+
+        int width = size, height = size;
+        if (preserveAspectRatio)
+        {
+            if (inputBitmap.Width > inputBitmap.Height)
+                height = (int)(((float)inputBitmap.Height / inputBitmap.Width) * size);
+            else
+                width = (int)(((float)inputBitmap.Width / inputBitmap.Height) * size);
+        }
+
+        var newBitmap = new Bitmap(inputBitmap, new Size(width, height));
+
+        // save the resized png into a memory stream for future use
+        using var memoryStream = new MemoryStream();
+        newBitmap.Save(memoryStream, ImageFormat.Png);
+
+        var iconWriter = new BinaryWriter(output);
+
+        // 0-1 reserved, 0
+        iconWriter.Write((byte)0);
+        iconWriter.Write((byte)0);
+
+        // 2-3 image type, 1 = icon, 2 = cursor
+        iconWriter.Write((short)1);
+
+        // 4-5 number of images
+        iconWriter.Write((short)1);
+
+        // image entry 1
+        // 0 image width
+        iconWriter.Write((byte)width);
+        // 1 image height
+        iconWriter.Write((byte)height);
+
+        // 2 number of colors
+        iconWriter.Write((byte)0);
+
+        // 3 reserved
+        iconWriter.Write((byte)0);
+
+        // 4-5 color planes
+        iconWriter.Write((short)0);
+
+        // 6-7 bits per pixel
+        iconWriter.Write((short)32);
+
+        // 8-11 size of image data
+        iconWriter.Write((int)memoryStream.Length);
+
+        // 12-15 offset of image data
+        iconWriter.Write(6 + 16);
+
+        // write image data
+        // png data must contain the whole png data file
+        iconWriter.Write(memoryStream.ToArray());
+
+        iconWriter.Flush();
+
+        return true;
+    }
+
+    /// <summary>
+    /// Converts a PNG image to a icon (ico)
+    /// </summary>
+    /// <param name="inputPath">The input path</param>
+    /// <param name="outputPath">The output path</param>
+    /// <param name="size">Needs to be a factor of 2 (16x16 px by default)</param>
+    /// <param name="preserveAspectRatio">Preserve the aspect ratio</param>
+    /// <returns>Wether or not the icon was succesfully generated</returns>
+    public static bool ConvertToIcon(string inputPath, string outputPath, int size = 16, bool preserveAspectRatio = false)
+    {
+        using (FileStream inputStream = new FileStream(inputPath, FileMode.Open))
+        using (FileStream outputStream = new FileStream(outputPath, FileMode.Create))
+        {
+            return ConvertToIcon(inputStream, outputStream, size, preserveAspectRatio);
+        }
+    }
+}
---
+++ FolderIconifier/MainWindow.xaml.cs
@@ -0,0 +1,165 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.Win32;
+using Vanara.PInvoke;
+using Path = System.IO.Path;
+
+namespace FolderIconifier;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        IconList.Items.Filter = ItemsFilter;
+        
+        var dataContext = new MainWindowViewModel();
+        DataContext = dataContext;
+        
+        foreach (var file in Directory.GetFiles(
+                     Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? "", "Icons"),
+                     "*.ico",
+                     SearchOption.AllDirectories))
+        {
+            dataContext.FolderIcons.Add(new FolderIcon(Path.GetFileNameWithoutExtension(file), file));
+        }
+    }
+
+    private bool ItemsFilter(object item)
+    {
+        return (item as FolderIcon)?.Name.Contains(FilterText.Text, StringComparison.InvariantCultureIgnoreCase) ?? false;
+    }
+
+    private void HandleDoubleClick(object sender, MouseButtonEventArgs e)
+    {
+        if (((ListViewItem) sender).Content is FolderIcon folderIcon)
+        {
+            foreach (var folder in Environment.GetCommandLineArgs())
+            {
+                if (Directory.Exists(folder))
+                {
+                    Colorize(folder, folderIcon);
+                }
+            }
+
+            Console.WriteLine(folderIcon.FilePath);
+        }
+    }
+    
+    private void Colorize(string folder, FolderIcon folderIcon)
+    {
+        var folderSettings = new Shell32.SHFOLDERCUSTOMSETTINGS
+        {
+            dwMask = Shell32.FOLDERCUSTOMSETTINGSMASK.FCSM_ICONFILE,
+            pszIconFile = folderIcon.FilePath,
+            dwSize = (uint)Marshal.SizeOf(typeof(Shell32.SHFOLDERCUSTOMSETTINGS)),
+            cchIconFile = 0
+        };
+        //FolderSettings.iIconIndex = 0;
+        var result = Shell32.SHGetSetFolderCustomSettings(ref folderSettings, folder, Shell32.FCS.FCS_FORCEWRITE);
+        if (!result.Succeeded)
+        {
+            throw new InvalidOperationException("Shell32.SHGetSetFolderCustomSettings failed.");
+        }
+        
+        Shell32.SHChangeNotify(Shell32.SHCNE.SHCNE_UPDATEDIR, Shell32.SHCNF.SHCNF_PATHW, folder);
+
+        
+//         var fileInfo = new FileInfo(Path.Combine(folder, "desktop.ini"));
+//         if (fileInfo.Exists)
+//         {
+//             fileInfo.Attributes &= ~(FileAttributes.Hidden | FileAttributes.System); // HSA
+//         }
+//         using (var stream = fileInfo.CreateText())
+//         {
+//             stream.Write(
+//                 $"""
+//                 [.ShellClassInfo]
+//                 IconResource={folderIcon.FilePath},0
+//                 """);
+//         }
+//         fileInfo.Attributes |= FileAttributes.Hidden | FileAttributes.System | FileAttributes.Archive; // HSA
+//         new DirectoryInfo(folder).Attributes |= FileAttributes.ReadOnly;
+//         Thread.Sleep(1000);
+//         SHChangeNotify(0x08000000, 0x0000, (IntPtr)null, (IntPtr)null);
+    }
+
+    private void FilterText_OnTextChanged(object sender, TextChangedEventArgs e)
+    {
+        IconList.Items.Filter = ItemsFilter;
+    }
+
+    private void Install_OnClick(object sender, RoutedEventArgs e)
+    {
+        if (Environment.ProcessPath == null) throw new InvalidOperationException("No process path provided.");
+        
+        using var superKey = Registry.CurrentUser.CreateSubKey(@"SOFTWARE\Classes\Directory\shell\FolderIconifier", true);
+        if (superKey == null) throw new InvalidOperationException();
+        superKey.SetValue("", "Colorize Folder", RegistryValueKind.String);
+        superKey.SetValue("Icon", $"\"{Environment.ProcessPath}\",0", RegistryValueKind.String);
+        using var commandKey = superKey.CreateSubKey("command");
+        if (commandKey == null) throw new InvalidOperationException();
+        commandKey.SetValue("", $"{Environment.ProcessPath} \"%1\"", RegistryValueKind.String);
+    }
+
+    private void Uninstall_OnClick(object sender, RoutedEventArgs e)
+    {
+        Registry.CurrentUser.DeleteSubKeyTree(@"SOFTWARE\Classes\Directory\shell\FolderIconifier", false);
+    }
+}
+
+public partial class MainWindowViewModel : ObservableObject
+{
+    [ObservableProperty] public partial FolderIcon? SelectedFolderIcon { get; set; }
+
+    public ObservableCollection<FolderIcon> FolderIcons { get; } = new();
+
+}
+
+public partial class FolderIcon : ObservableObject
+{
+    public string FilePath { get; }
+
+    [ObservableProperty] public partial string Name { get; set; }
+    [ObservableProperty] public partial ImageSource ImageSource { get; set; }
+
+    public FolderIcon(string name, string filePath)
+    {
+        Name = name;
+
+        FilePath = filePath;
+        
+        var bitmapImage = new BitmapImage();
+        bitmapImage.BeginInit();
+
+        using (var stream = File.OpenRead(filePath))
+        {
+            bitmapImage.StreamSource = stream;
+            bitmapImage.DecodePixelHeight = 32;
+            bitmapImage.DecodePixelWidth = 32;
+
+            bitmapImage.EndInit();
+        }
+
+        ImageSource = bitmapImage;
+    }
+}
